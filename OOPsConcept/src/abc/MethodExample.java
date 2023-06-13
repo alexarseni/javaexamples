1678ff474475e85592587c5887b0094982f0ea3e
AlexandraArseni <alexarseni@outlook.com>
2023-06-13 11:13:40 +0000

    OOPs method and string examples

--- OOPsConcept/src/abc/MethodExample.java
+++ OOPsConcept/src/abc/MethodExample.java
@@ -10,11 +10,22 @@ public class MethodExample {
 		int sum = x+y;
 		System.out.println("sum of two numbers is "+sum);
 	}
+	
+	static int findLargest(int x, int y) {
+		if(x>y) {
+			return x;
+		}
+		else {
+			return y;
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		display();
 		add(100,200);
+		int result = findLargest(5,3);
+		System.out.println("The largest number is "+result);
 	}
 
 }
