567685775fb1b0a9c4e3a043666bdd116e5d4707
AlexandraArseni <alexarseni@outlook.com>
2023-07-24 12:01:49 +0000

    Exception Handling 10-07 2

--- ExceptionHandling/src/com/RuntimeException.java
+++ ExceptionHandling/src/com/RuntimeException.java
@@ -5,16 +5,20 @@ public class RuntimeException {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi");
+		int res[] = {100,200,300};
 		int a = 10, b=0;
 		try {
 		int result = a/b;
+		int result1 = 100/res[5];
+		
 		System.out.println("Result "+result); //we will get arithmetic exception because of the divison by zero
+		System.out.println("Result "+result1);
 		}catch(Exception e) {
-			System.out.println("we are catching any exception");
-			System.out.println(e.getMessage());
-			System.out.println(e.getCause());
-			System.out.println(e.getClass());
-			System.out.println(e.toString());
+			System.out.println("we are catching any  type ofexception");
+			//System.out.println(e.getMessage());
+		//	System.out.println(e.getCause());
+			//System.out.println(e.getClass());
+			System.out.println(e.toString()); //name fo the exception and message
 		}
 		System.out.println("Bye");
 	}
