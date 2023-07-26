4c7a12068d68e5ea372c654db7386ddad2ef8aed
AlexandraArseni <alexarseni@outlook.com>
2023-07-26 06:21:35 +0000

    Multithreading 11-07

--- MultithreadingExample/src/abc/ExtendsthreadClassExample.java
+++ MultithreadingExample/src/abc/ExtendsthreadClassExample.java
@@ -1,18 +1,36 @@
 package abc;
 
 class A extends Thread{
-	
+	@Override
+	//it is not mandatory to override the run method, because itexists in the super class
+	public void run() {
+		//super.run(); this calls the run method of the super class ie the thread class
+		for (int i = 0; i<=10;i++) {
+			System.out.println("i "+i);
+		}
+	}
 }
 
 class B extends Thread{
-	
+	@Override
+	public void run() {
+		//super.run();
+		for (int j = 0; j<=10;j++) {
+			System.out.println("j "+j);
+		}
+	}
 }
 
 public class ExtendsthreadClassExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		A obj1 = new A();
+		B obj2 = new B();
+		//if these start methods were non-thread methods, they would be
+		//executed sequentially ie. the first one would finish before the
+		//second one started. threads DONT need to wait. 
+		obj1.start(); //after calling the start method, the thread goes into 
+		//the runnable state - ready to run
+		obj2.start(); //threads don't need to wait for the other to complete
 	}
-
 }
---
+++ MultithreadingExample/src/xyz/ImplementsRunnableInterface.java
@@ -0,0 +1,25 @@
+package xyz;
+
+class A implements Runnable {
+	@Override
+	//it is mandatory to override the run method, because it is part of the interface
+	public void run() {
+		
+	}
+}
+
+class B implements Runnable {
+	@Override
+	public void run() {
+		
+	}
+}
+
+public class ImplementsRunnableInterface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
