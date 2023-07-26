5db174924d6e315c7b278fe173c60779a388b957
AlexandraArseni <alexarseni@outlook.com>
2023-07-26 10:01:27 +0000

    Booking App in multithreading

--- MultithreadingExample/src/com/BookingApp.java
+++ MultithreadingExample/src/com/BookingApp.java
@@ -1,21 +1,28 @@
 package com;
 
 class Booking implements Runnable {
+	//in this scenario, only one ticket is available
 	int av1 =1;
 
 	@Override
-	public void run() {
+	//if the method is declared as synchronized, once the first thread enters the 
+	//method, no other thread will be allowed to start the execution of the method's
+	//code. Lex will always get the ticket
+	public synchronized void run() {
 		// TODO Auto-generated method stub
 		Thread t = Thread.currentThread();
 		String name = t.getName();
+		//the problem is that if the threads are checking simultaneously the
+		//value of av1, they will all get the ticket. We need to control the use
+		//of the resources, so that only one thread at a time can access av1.
+		
 		if (av1==1) {
 			System.out.println(name + " got the ticket");
 			av1--;
 		}
 		else {
-			System.out.println("sorry, no ticket");
+			System.out.println(name + " sorry, no ticket");
 		}
-		
 		
 	}	
 }
@@ -23,14 +30,14 @@ class Booking implements Runnable {
 public class BookingApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Only one booking is available
 		Booking b1 = new Booking();
-		Booking b2 = new Booking();
-		Booking b3 = new Booking();
+		//Booking b2 = new Booking();
+		//Booking b3 = new Booking();
 		
 		Thread t1 = new Thread(b1);
-		Thread t2 = new Thread(b2);
-		Thread t3 = new Thread(b3);
+		Thread t2 = new Thread(b1);
+		Thread t3 = new Thread(b1);
 		
 		t1.setName("Lex");
 		t2.setName("Vijay");
