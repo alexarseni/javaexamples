cde0b9432ddb76183584984ef95845a9c48286e4
AlexandraArseni <alexarseni@outlook.com>
2023-07-27 09:47:51 +0000

    Queue Demo'

---
+++ CollectionFrameworkExamples/src/com/QueueDemo.java
@@ -0,0 +1,43 @@
+package com;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class QueueDemo {
+
+	public static void main(String[] args) {
+		Queue q1 = new LinkedList(); //interface Queue, class linkedList
+		Queue q2 = new PriorityQueue();
+		
+		q1.add(3);
+		q1.add(1);
+		q1.add(4);
+		q1.add(5);
+		q1.add(4);
+		
+		q2.add(3);
+		q2.add(1);
+		q2.add(4);
+		q2.add(5);
+		q2.add(4);
+		
+		
+		//The linkedList queue maintains the insertion order
+		System.out.println("LinkedList queue: "+q1);
+		
+		//PriorityQueue automatically orders the elements based on priority
+		System.out.println("PriorityQueue: "+q2);
+		q1.poll();
+		q2.poll();
+		System.out.println("First poll");
+		System.out.println(q1+"\n"+q2);
+		
+		q1.poll();
+		q2.poll();
+		System.out.println("Second poll");
+		System.out.println(q1+"\n"+q2);
+		
+	}
+
+}
---
+++ CollectionFrameworkExamples/src/com/VectorDemo.java
@@ -0,0 +1,16 @@
+package com;
+
+import java.util.Vector;
+
+public class VectorDemo {
+
+	public static void main(String[] args) {
+		Vector v = new Vector();
+		v.add(10); //new method
+		v.addElement(20); //old add method
+		v.add(30);
+		System.out.println(v);
+
+	}
+
+}
