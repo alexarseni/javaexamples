da27b00ff10c1d775dd4fe71d171522c0969bac8
AlexandraArseni <alexarseni@outlook.com>
2023-07-28 09:51:16 +0000

    ArraysDemo 2

--- CollectionFrameworkExamples/src/abc/ArraysDemo.java
+++ CollectionFrameworkExamples/src/abc/ArraysDemo.java
@@ -23,6 +23,30 @@ public class ArraysDemo {
 		for (String n:names) {
 			System.out.print(n+ " ");
 		}
+		System.out.println();
+		
+		//To use binary search elements need to have been sorted first.
+		int num2[] = {8,10,3,7,3,1,12}; //{1,3,3,7,8,10,12}
+		
+		System.out.println("\nDisplay elements before sort: ");
+		for (int n:num2) {
+			System.out.print(n+ " ");
+		}
+		//this answer will be wrong
+		System.out.println("\nSearch Result before sorting:");
+		System.out.println(Arrays.binarySearch(num2, 1)); //wrong answer
+		//to use binary search the array must first be sorted
+		
+		Arrays.sort(num2);
+		System.out.println("Search Result after sorting:");
+		System.out.println(Arrays.binarySearch(num2, 1)); //right answer
+		
+		if(Arrays.binarySearch(num2, 20)<0) {
+			System.out.println("Not present");
+		}
+		else {
+			System.out.println("Element present");
+		}
 		
 	}
 	
