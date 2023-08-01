82d62997a7eb5ae2bec5cdeacf8e1307c5ebce06
AlexandraArseni <alexarseni@outlook.com>
2023-08-01 08:52:16 +0000

    Exception service

---
+++ ProductManagementSystem/src/pms/exception/ProductPriceException.java
@@ -0,0 +1,13 @@
+package pms.exception;
+
+public class ProductPriceException extends Exception{
+	public ProductPriceException() {
+		
+	}
+	
+	public ProductPriceException(String msg) {
+		super(msg);
+	
+	}
+	
+}
--- ProductManagementSystem/src/pms/service/ProductService.java
+++ ProductManagementSystem/src/pms/service/ProductService.java
@@ -1,10 +1,9 @@
 package pms.service;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Iterator;
-
 import pms.bean.Product;
+import pms.exception.ProductPriceException;
 
 public class ProductService {
 
@@ -14,29 +13,45 @@ private List<Product> listOfProducts = new ArrayList<Product>(); //i do not want
 public String storeProduct(Product product) {
 	
 	int flag=0; //make sure flag is zero everytime we run this code to add a new product
-	if(listOfProducts.size()==0) {
-		listOfProducts.add(product);
-		return "Product details stored succcessfully";
-	}else {
-		Iterator<Product> li = listOfProducts.iterator();
-		while(li.hasNext()) {
-			Product p = li.next();
-			//if the inserted pid is equal to any other pid already inserted then the item should be discarded
-			if(p.getPid()==product.getPid()) { //list allows for duplicates
-				flag++; //parsing the Arraylist elements one by one
-				break;
+	try {
+		if(listOfProducts.size()==0) {
+			if(product.getPrice()<0) {
+				throw new ProductPriceException("Price must be >0");
+			}
+			else {
+				listOfProducts.add(product);
+				return "Product details stored succcessfully";
 			}
 		}
+			
+		else {
+			Iterator<Product> li = listOfProducts.iterator();
+			while(li.hasNext()) {
+				Product p = li.next();
+				//if the inserted pid is equal to any other pid already inserted then the item should be discarded
+				if(p.getPid()==product.getPid()) { //list allows for duplicates
+					flag++; //parsing the Arraylist elements one by one
+					break;
+				}
+			}
+		}
+ //we may have to raise an exception if the user has input a duplicate id or a negative price
+		if(flag!=0) {
+			return "product id must be unique";
+		}else if(product.getPrice()<0){
+			throw new ProductPriceException("Price must be > 0");
+		}
+		else{
+			listOfProducts.add(product);
+			//flag=0;
+			return "Product details stored succcessfully"; //we reutrn it, not print it, because we do not want to interact with
+			//input/output devices
+		}
+		
 	}
-	if(flag!=0) {
-		return "product id must be unique";
-	}else {
-		listOfProducts.add(product);
-		//flag=0;
-		return "Product details stored succcessfully"; //we reutrn it, not print it, because we do not want to interact with
-		//input/output devices
+	catch (Exception e) {
+		return e.toString();
 	}
-
 }
 
 public int numberOfProduct() {
