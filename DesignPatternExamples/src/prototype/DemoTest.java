86c92e989704703b42959ab58827120080b07f8d
AlexandraArseni <alexarseni@outlook.com>
2023-08-02 10:02:48 +0000

    DesignPatternExamples

--- DesignPatternExamples/src/prototype/DemoTest.java
+++ DesignPatternExamples/src/prototype/DemoTest.java
@@ -1,7 +1,12 @@
 package prototype;
 
-class Employee implements Cloneable{
+class Employee implements Cloneable{// the interface cloneable does not contain any methods
+	//it acts as a permission to use the clone method.
 	double salary;
+	public Employee() {
+		System.out.println("Object creation through the constructor");
+	}
+	
 	public void display() {
 		System.out.println("Salary is "+salary);
 	}
@@ -9,8 +14,10 @@ class Employee implements Cloneable{
 		try {
 			Object obj = clone(); //the clone method is predefined from the object class and throws exception CloneNotSupported
 			//it returns an object of type Object
+			//we cannot call the clone method and store the result in any other class's object other than  Object 
 			Employee e = (Employee)obj; //downlevel typecasting: create an Employee object from the super class Object
-			//the class that we want to have the cloning functionality, we need to declare that it implements Clonable
+			//the class that we want to have the cloning functionality, we need to declare that it implements Cloneable
+			System.out.println("Object creation through the getClone method");
 			return e;
 		}
 		catch(Exception e) {
@@ -41,8 +48,13 @@ public class DemoTest {
 		emp4.display();
 		emp2.display();
 		emp2.salary = 1500;
+		System.out.println("After changing emp2 salary");
 		emp4.display();
 		emp2.display();
+		System.out.println(emp1);
+		System.out.println(emp2);
+		System.out.println(emp3);
+		System.out.println(emp4);
 		
 		
 
