cec257825c6d622a8370700a9da7bda1d9b4001b
AlexandraArseni <alexarseni@outlook.com>
2023-08-02 11:45:05 +0000

    FileHandling 2

--- FileHandling/src/com/BytewiseOperations.java
+++ FileHandling/src/com/BytewiseOperations.java
@@ -1,6 +1,8 @@
 package com;
 
 import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.PrintStream;
 
 public class BytewiseOperations {
@@ -18,6 +20,25 @@ public class BytewiseOperations {
 		ps.println("Enter the name");
 		String name = dis.readLine(); //deprecated and exception
 		ps.println("Name is "+name);
+		
+		//source: keyboard
+		//destination: file
+		//FileOutputStream fos = new FileOutputStream("abc.txt");  //if the file does not exist, it will create it
+		FileOutputStream fos = new FileOutputStream("abc.txt", true);//appends at the end of the file
+		//if it does exist, it will override it
+		System.out.println("Please enter the data");
+		int ch;
+		while((ch = dis.read()) != '@') {
+			System.out.println(ch+ "=" + (char)ch);
+			fos.write(ch);
+		}
+		fos.close();
+		System.out.println("Data stored in file");
+		
+		FileInputStream fi = new FileInputStream("abc.txt");
+		int data = fi.read(); //will read first character
+		System.out.println("The byte read is "+ (char)data);
+		
 	}
 
 }
